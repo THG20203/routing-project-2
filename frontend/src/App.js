fd689a051a8b1397ca6fc6ba72b4c456b5ae89db
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-11 20:29:44 +0100

    router dom added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,13 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//create router
+const router = createBrowserRouter([
+  // root route, element is the homepage
+  { path: "/", element: <HomePage /> },
+]);
 
 function App() {
   return <div></div>;
