d06d6068790b43ec6636cc3e8509784280296254
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-13 22:36:11 +0100

    highlighting the active link

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
@@ -8,11 +8,27 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            {/* using absolute path  - always want to go to home page */}
-            <Link to="/">Home</Link>
+            {/* using absolute path  - always want to go to home page. Navlink recieves a 
+            function that gets an object, automatically provided by react router, where we 
+            can get the is active class. */}
+            <NavLink
+              to="/"
+              classname={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/events">Events</Link>
+            <NavLink
+              to="/events"
+              classname={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Events
+            </NavLink>
           </li>
         </ul>
       </nav>
