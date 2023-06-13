0c99ced7ed8c1416b170d0f907eb9aed75d09fcf
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-13 23:10:38 +0100

    useparams event details

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,18 @@
+/* useParams hook -> gives us access to the currently active router parameters, so
+to the values that are encoded in the url for our dynamic path segments. */
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>EventDetailPage</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      {/* its .eventId in my case cause I used eventId in my app.js as an identifier
+      after the colon. */}
+      <p>EventId: {params.eventId}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
