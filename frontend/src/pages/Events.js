d06b1c42369aa2b9cf93eafbedb17268876a846a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-14 00:02:19 +0100

    comments added to events.js

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -10,11 +10,14 @@ function EventsPage() {
   useEffect(() => {
     async function fetchEvents() {
       setIsLoading(true);
+      // sends https request to dummy backend API
       const response = await fetch("http://localhost:8080/events");
 
+      // error stored if invalid response
       if (!response.ok) {
         setError("Fetching events failed.");
       } else {
+        // if no error extract the data from the response -> store that with the help of state
         const resData = await response.json();
         setFetchedEvents(resData.events);
       }
@@ -25,6 +28,9 @@ function EventsPage() {
   }, []);
   return (
     <>
+      {/* all the states used down here to show a Loading text, an error message or the fetch events 
+    which are now fetched from that dummy backend. Those events are rendered here with the help of that 
+    list component in the components folder. */}
       <div style={{ textAlign: "center" }}>
         {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>}
