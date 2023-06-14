af4c2c331a4887434124d20dc82a0ced711aba99
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-14 10:30:10 +0100

    using data from a loader in the router component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,17 @@
+/* useLoaderData is a special hook which we can execute to get access to the closest loader data. */
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
 function EventsPage() {
-  /* How do we now get access to that data that is returned by our loader */
+  /* How do we now get access to that data that is returned by our loader? First
+  above importing useLoaderData */
+  const events = useLoaderData();
 
   return (
     <>
-      <EventsList />
+      {/* the events object / the array of events which I can pass as a value to the events prop
+      on Events list. */}
+      <EventsList events={events} />
     </>
   );
 }
