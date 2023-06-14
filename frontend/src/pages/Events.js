0eb8117e07cb03f7f3555074fe379f5e5aa10a7a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-14 10:04:14 +0100

    removing react code from event.js

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
-
-import EventsList from "../components/EventsList";
-
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
+  /* How do we now get access to that data that is returned by our loader */
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
   return (
     <>
-      {/* all the states used down here to show a Loading text, an error message or the fetch events 
-    which are now fetched from that dummy backend. Those events are rendered here with the help of that 
-    list component in the components folder. */}
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList />
     </>
   );
 }
