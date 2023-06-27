830048e3bc6e70375c2e075e97f42e3e4666a0af
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-28 00:26:25 +0100

    getting ready for reusing actions via request methods

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -82,3 +82,36 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  /* sending a request with the fetch function - to localhost:8080 - to my dummy backend API which 
+  listens on port 8080/events */
+  /* also want to send a post request, and add some data to the request. Data that I want to send 
+  is data submitted with the form. On form all inputs should have name attribute */
+  const response = fetch("https://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    // if 422 status code is present, want to return a response
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event" }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
